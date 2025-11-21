7003935083a5161c4f868df77e786b1fd0ab28a1
oalt <oa@lacheweg21.de>
2025-11-21 15:47:13 +0100

    Adding a simple test project

---
+++ src/EmofGeneratorTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using MDD4All.EMOF.DataModels;
+using MDD4All.EMOF.DotNetToEmofConverter;
+using MDD4All.Person.DataModels;
+using Newtonsoft.Json;
+
+namespace EmofGeneratorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DotNetToEmofConverter dotNetToEmofConverter = new DotNetToEmofConverter();
+
+            EmofRepository emofRepository = dotNetToEmofConverter.ConvertToEMOF(typeof(Repository));
+
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings();
+
+            string json = JsonConvert.SerializeObject(emofRepository, 
+                                                      Formatting.Indented, 
+                                                      new JsonSerializerSettings()
+                                                      {
+                                                          NullValueHandling = NullValueHandling.Ignore,
+                                                          TypeNameHandling = TypeNameHandling.Auto
+                                                      });
+
+            File.WriteAllText("..\\..\\..\\emofModel.json", json);
+        }
+    }
+}
